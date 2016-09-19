c33c9bafb4c4564e6c0bce61803f3ff95bc3e9d6
Jerry He <jerryhe9500@gmail.com>
2016-09-19 22:48:47 +0800

    BasicCopy
    Add basic copy function.

--- FTPUploder/MainForm.cs
+++ FTPUploder/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
 
 namespace FTPUploder
 {
@@ -15,6 +16,57 @@ namespace FTPUploder
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, EventArgs e)
+        {
+            //Choose a sourcee file
+            do
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        if (openFileDialog.OpenFile() != null)
+                        {
+                            fileName = openFileDialog.SafeFileName;
+                            sourceFile = openFileDialog.FileName;
+                            saveFileDialog.FileName = fileName;
+                            MessageBox.Show("source:" + sourceFile);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                    }
+                }
+            } while (sourceFile == null);
+            //Choose a destination path
+            do
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        destFile = saveFileDialog.FileName;
+                        MessageBox.Show("destination:" + destFile);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Error: Could not save file to disk. Original error: " + ex.Message);
+                    }
+                }
+            } while (destFile == null);
+
+            //Provide a standard dialog box that shows progress on file operations in Windows
+            try
+            {
+                FileSystem.CopyFile(sourceFile, destFile, UIOption.AllDialogs);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
